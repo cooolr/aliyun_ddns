db3aa6a29d80674a131f6f02123be0a4964ebe95
liyr <i@lr.cool>
2020-07-31 14:37:30 +0800

    only supports python3

--- aliyun_ddns.py
+++ aliyun_ddns.py
@@ -1,124 +1,127 @@
 # -*- coding: utf-8 -*-
 
 import os
-import uuid
-import json
-import requests
 import re
-from datetime import datetime
+import hmac
+import json
+import uuid
+import base64
 import urllib
 import hashlib
-import hmac
+import logging
+import requests
+import datetime
 
-from bs4 import BeautifulSoup as BS
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 REQUEST_URL = 'https://alidns.aliyuncs.com/'
-LOCAL_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ip.txt')
-ALIYUN_SETTINGS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'aliyun_settings.json')
+LAST_IP = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ip.txt')
+SETTINGS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'aliyun_settings.json')
 
 
 def get_common_params(settings):
-	"""
-	获取公共参数
-	参考文档：https://help.aliyun.com/document_detail/29745.html?spm=5176.doc29776.6.588.sYhLJ0
-	"""
-	return {
-		'Format': 'json',
-		'Version': '2015-01-09',
-		'AccessKeyId': settings['access_key'],
-		'SignatureMethod': 'HMAC-SHA1',
-		'Timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
-		'SignatureVersion': '1.0',
-		'SignatureNonce': uuid.uuid4()
-	}
+    '''
+    获取公共参数
+    参考文档：https://help.aliyun.com/document_detail/29745.html?spm=5176.doc29776.6.588.sYhLJ0
+    '''
+    return {
+        'Format': 'json',
+        'Version': '2015-01-09',
+        'AccessKeyId': settings['access_key'],
+        'SignatureMethod': 'HMAC-SHA1',
+        'Timestamp': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        'SignatureVersion': '1.0',
+        'SignatureNonce': uuid.uuid4()
+    }
+
 
 def get_signed_params(http_method, params, settings):
-	"""
-	参考文档：https://help.aliyun.com/document_detail/29747.html?spm=5176.doc29745.2.1.V2tmbU
-	"""
+    '''
+    参考文档：https://help.aliyun.com/document_detail/29747.html?spm=5176.doc29745.2.1.V2tmbU
+    '''
+    #1、合并参数，不包括Signature
+    params.update(get_common_params(settings))
+    #2、按照参数的字典顺序排序
+    sorted_params = sorted(params.items())
+    #3、encode 参数
+    query_params = urllib.parse.urlencode(sorted_params)
+    #4、构造需要签名的字符串
+    str_to_sign = http_method + '&' + urllib.parse.quote_plus('/') + '&' + urllib.parse.quote_plus(query_params)
+    #5、计算签名
+    signature = hmac.new((settings['access_secret'] + '&').encode('utf-8'), (str_to_sign).encode('utf-8'), hashlib.sha1).digest()
+    signature = base64.b64encode(signature).decode()
+    #6、将签名加入参数中
+    params['Signature'] = signature
+    return params
 
-	#1、合并参数，不包括Signature
-	params.update(get_common_params(settings))
-	#2、按照参数的字典顺序排序
-	sorted_params = sorted(params.items())
-	#3、encode 参数
-	query_params = urllib.urlencode(sorted_params)
-	#4、构造需要签名的字符串
-	str_to_sign = http_method + "&" + urllib.quote_plus("/") + "&" + urllib.quote_plus(query_params)
-	#5、计算签名
-	signature = hmac.new(str(settings['access_secret'] + '&'), str(str_to_sign), hashlib.sha1).digest().encode('base64').strip('\n') #此处注意，必须用str转换，因为hmac不接受unicode，大坑！！！
-	#6、将签名加入参数中
-	params['Signature'] = signature
 
-	return params
+def update_resolution(ip):
+    '''
+    修改云解析
+    参考文档：
+        获取解析记录：https://help.aliyun.com/document_detail/29776.html?spm=5176.doc29774.6.618.fkB0qE
+        修改解析记录：https://help.aliyun.com/document_detail/29774.html?spm=5176.doc29774.6.616.qFehCg
+    '''
+    with open(SETTINGS, 'r') as f:
+        settings = json.loads(f.read())
 
-def update_yun(ip):
-	"""
-	修改云解析
-	参考文档：
-		获取解析记录：https://help.aliyun.com/document_detail/29776.html?spm=5176.doc29774.6.618.fkB0qE
-		修改解析记录：https://help.aliyun.com/document_detail/29774.html?spm=5176.doc29774.6.616.qFehCg
-	"""
-	with open(ALIYUN_SETTINGS, 'r') as f:
-		settings = json.loads(f.read())
+    domain = settings['domain']
+    root_domain = '.'.join(domain.split('.')[1:])
+    pre_domain = domain.split('.')[0]
 
-	#首先获取解析列表
-	get_params = get_signed_params('GET', {
-		'Action': 'DescribeDomainRecords',
-		'DomainName': settings['domain'],
-		'TypeKeyWord': 'A'
-	}, settings)
+    #首先获取解析列表
+    get_params = get_signed_params('GET', {
+        'Action': 'DescribeDomainRecords',
+        'DomainName': root_domain,
+        'TypeKeyWord': 'A'
+    }, settings)
+    logging.info('get current resolution params')
+    get_resp = requests.get(REQUEST_URL, get_params)
+    records = get_resp.json()
+    if records.get('Message'):
+        logging.error(records['Message'])
+        exit()
 
-	get_resp = requests.get(REQUEST_URL, get_params)
+    for record in records['DomainRecords']['Record']:
+        if record['RR'] != pre_domain:
+            continue
+        logging.info('signed resolution params')
+        post_params = get_signed_params('POST', {
+            'Action': 'UpdateDomainRecord',
+            'RecordId': record['RecordId'],
+            'RR': record['RR'],
+            'Type': record['Type'],
+            'Value': ip
+        }, settings)
+        post_resp = requests.post(REQUEST_URL, post_params)
+        result = post_resp.json()
+        flag = "successfully" if not result.get('Message') else f'failed: {result["Message"]}'
+        logging.info(f'update resolution {flag}')
 
-	records = get_resp.json()
-	print 'get_records============'
-	print records
-	for record in records['DomainRecords']['Record']:
-		post_params = get_signed_params('POST', {
-			'Action': 'UpdateDomainRecord',
-			'RecordId': record['RecordId'],
-			'RR': record['RR'],
-			'Type': record['Type'],
-			'Value': ip
-		}, settings)
-		post_resp = requests.post(REQUEST_URL, post_params)
-		result = post_resp.json()
-		print 'update_record============'
-		print result
 
 def get_curr_ip():
-	headers = {
-		'content-type': 'text/html',
-		'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'
-	}
-	resp = requests.get('https://www.ip.cn/', headers=headers)
-	soup = BS(resp.content, 'html.parser')
-	for t in soup.find_all('code'):
-		if re.search(r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", t.string):
-			return t.string
-	return ''
+    headers = {
+        'Cookie': 'BIDUPSID=767700D726476D09385B464D6208BF86; PSTM=1596177058; BAIDUID=767700D726476D0975CC602058186A99:FG=1; H_WISE_SIDS=152083_152477_150724_152360_150685_149356_150076_147090_150085_151594_148867_150796_147279_152309_150043_149812_151017_146573_148523_151032_127969_148794_149719_146653_151319_146732_145784_152740_150437_131423_152023_151389_152696_147588_151147_107311_152560_152274_149253_151220_152284_144966_152270_152513_139883_146786_149771_152249_147546_148869_151704_110085; delPer=0; BDSVRTM=53; PSINO=6; ysm=1847; __bsi=9304608067065090910_00_24_N_N_84_0303_c02f_Y; BDSVRBFE=Go'
+    }
+    r = requests.get('https://m.baidu.com/s?wd=ip', headers=headers)
+    ip = re.findall('"ip":"(.*?)"', r.text)[0]
+    logging.info(f'get current ip  {ip}')
+    return ip
 
-def get_lastest_local_ip():
-	"""
-	获取最近一次保存在本地的ip
-	"""
-	print 'ip local path', LOCAL_FILE
-	with open(LOCAL_FILE, 'w+') as f:
-		last_ip = f.readline()
-	return last_ip
+
+def get_lastest_ip():
+    if os.path.exists(LAST_IP):
+        with open(LAST_IP, 'r') as f:
+            ip = f.read()
+        logging.info(f'get the last ip {ip}')
+        return ip
+
 
 if __name__ == '__main__':
-	ip = get_curr_ip()
-	if not ip:
-		print 'get ip failed'
-	else:
-		last_ip = get_lastest_local_ip()
-		print ip, last_ip
-		if ip != last_ip:
-			print 'save ip to {}...'.format(LOCAL_FILE)
-			with open(LOCAL_FILE, 'wb') as f:
-				f.write(ip)
-			print 'update remote record...'
-			update_yun(ip)
+    curr_ip = get_curr_ip()
+    last_ip = get_lastest_ip()
+    if curr_ip != last_ip:
+        update_resolution(curr_ip)
+
+        with open(LAST_IP, 'w') as f:
+            f.write(curr_ip)
